2e902c7ee808102c37185faca12cf7797a4ba30d
AleksandrSoiko <farshchef777@gmail.com>
2023-03-28 18:39:50 +0100

    add notification message to form contact

--- src/components/FormsAdd/FormsAdd.jsx
+++ src/components/FormsAdd/FormsAdd.jsx
@@ -1,8 +1,10 @@
 import { Formik, Field } from 'formik';
 import { Form, FormField, SubmitBtn, ErrorMessage } from './FormsAdd.styled';
 import * as Yup from 'yup';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addContact } from 'redux/contactsSlice/contactsSlice';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ContactSchema = Yup.object().shape({
   name: Yup.string()
@@ -19,32 +21,44 @@ const ContactSchema = Yup.object().shape({
 
 export const ContactForm = () => {
   const dispatch = useDispatch();
+  const contacts = useSelector(state => state.contacts.phones);
+  const notify = () => toast('Such contact is already existed');
 
   return (
-    <Formik
-      initialValues={{
-        name: '',
-        number: '',
-      }}
-      validationSchema={ContactSchema}
-      onSubmit={(values, action) => {
-        dispatch(addContact(values));
-        action.resetForm();
-      }}
-    >
-      <Form>
-        <FormField>
-          Name
-          <Field name="name"></Field>
-          <ErrorMessage name="name" component="span" />
-        </FormField>
-        <FormField>
-          Number
-          <Field name="number"></Field>
-          <ErrorMessage name="number" component="span" />
-        </FormField>
-        <SubmitBtn type="submit">Add contact</SubmitBtn>
-      </Form>
-    </Formik>
+    <>
+      <ToastContainer />
+      <Formik
+        initialValues={{
+          name: '',
+          number: '',
+        }}
+        validationSchema={ContactSchema}
+        onSubmit={(values, action) => {
+          for (const contact of contacts) {
+            if (contact.number === values.number) {
+              notify();
+              action.resetForm();
+              return;
+            }
+          }
+          dispatch(addContact(values));
+          action.resetForm();
+        }}
+      >
+        <Form>
+          <FormField>
+            Name
+            <Field name="name"></Field>
+            <ErrorMessage name="name" component="span" />
+          </FormField>
+          <FormField>
+            Number
+            <Field name="number"></Field>
+            <ErrorMessage name="number" component="span" />
+          </FormField>
+          <SubmitBtn type="submit">Add contact</SubmitBtn>
+        </Form>
+      </Formik>
+    </>
   );
 };
